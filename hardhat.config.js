a35b4c3e97ef63e4784232630ad422e8b76a4d0b
mok <mok0230@protonmail.com>
2021-12-02 09:54:49 -0800

    remove kovan network from hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,6 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    kovan: {
-      url: `${process.env.ALCHEMY_KOVAN_URL}`,
-      accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`],
-    },
     rinkeby: {
       url: `${process.env.ALCHEMY_RINKEBY_URL}`,
       accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
